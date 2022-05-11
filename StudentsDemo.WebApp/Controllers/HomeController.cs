a34b516012b38226120f2fc4976af47104be2c25
Starinin Andrey <starininandrey@gmail.com>
2022-05-11 21:16:45 +0300

    Init

---
+++ StudentsDemo.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using StudentsDemo.WebApp.Models;
+
+namespace StudentsDemo.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ StudentsDemo.WebApp/Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsDemo.WebApp.Models;
+
+namespace StudentsDemo.WebApp.Controllers;
+
+public class StudentsController : Controller
+{
+    private List<Student> _students;
+
+    public StudentsController()
+    {
+        _students = new List<Student>
+        {
+            new() { Id = 0, FirstName = "Anna", LastName = "Karenina"},
+            new() { Id = 0, FirstName = "Anna", LastName = "Karenina"},
+        };
+    }
+
+    [Route("/students")]
+    public IActionResult Students()
+    {
+        return View("StudentsView", _students);
+    }
+
+    [Route("/student/{id:int}")]
+    public IActionResult Student(int id)
+    {
+        var student = _students.FirstOrDefault(s => s.Id == id);
+        return View("StudentView", student);
+    }
+}
